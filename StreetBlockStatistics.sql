baa12307d85c12e72a6ed69ddb936b32acae68d7
MichaelJendryke <michael@jendryke.de>
2015-09-05 16:12:00 +0800

    second

--- StreetBlockStatistics.sql
+++ StreetBlockStatistics.sql
@@ -49,21 +49,94 @@ ON
 WHERE
 	polygon.ADM_2 = 262
 
+USE [weiboDEV]
+
+GO
+
+CREATE UNIQUE CLUSTERED INDEX [ClusteredIndex-20150905-141653] ON [dbo].[LINK_msgID_userID_to_STREET_BLOCKS_Input_FID_Shanghai_262]
+(
+	[msgID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+
+GO
+
 
 ------------------------------------------------------------------------------------------------------
--- 2. Create Table that links msgID and objectID from STREET_BLOCK_DISSOLVED_THIESSEN_ID polygons  ---
+-- 2. Count RESIDETNS Message Count and Distict User Count from Shanghai  ---
 ------------------------------------------------------------------------------------------------------
+USE
+	weiboDEV
+GO
 SELECT
 	[Input_FID],
-	Count([msgID]) as MessageCount,
-	Count(Distinct [userID]) as UserCount
+	COUNT (LINK1.[msgID]) AS MessageCount,
+	Count(Distinct LINK1.[userID]) AS UserCount
+INTO
+	temp1
 FROM
-	LINK_msgID_userID_to_STREET_BLOCKS_Input_FID_Shanghai_262
+	LINK_msgID_userID_to_STREET_BLOCKS_Input_FID_Shanghai_262 as LINK1
 GROUP BY
 	[Input_FID]
 
 
+SELECT
+	[Input_FID],
+	COUNT (LINK1.[msgID]) AS ResidentsMessageCount,
+	Count(Distinct LINK1.[userID]) AS ResidentsUserCount
+INTO
+	temp2
+FROM
+	LINK_msgID_userID_to_STREET_BLOCKS_Input_FID_Shanghai_262 as LINK1
+JOIN
+	[dbo].[NBT4_exact_copy]
+ON
+	LINK1.msgID = [dbo].[NBT4_exact_copy].[msgID]
+WHERE
+	[dbo].[NBT4_exact_copy].userprovince = 31
+GROUP BY
+	[Input_FID]
 
+
+Select  
+	Polygon.[OBJECTID],
+	Polygon.[Input_FID],
+	[ADM_2],
+	CENSUS.POP as TotalPopulation,
+	CENSUS.M as MalePopulation,
+	CENSUS.F as FemalePopulation,
+	CENSUS.AGE0 as AGE0Population,
+	CENSUS.AGE15 as AGE15Population,
+	CENSUS.AGE65 as AGE65Population,
+	CENSUS.Address as PopulationAddress,
+	MessageCount,
+	MessageCount/CENSUS.POP as MessagesPerTotalPopulation
+	ResidentsMessageCount,
+	ResidentsMessageCount/CENSUS.POP as ResidentMessagesPerTotalPopulation
+	UserCount,
+	UserCount/CENSUS.POP as UsersPerTotalPopulation
+	ResidentsUserCount,
+	ResidentsUserCount/CENSUS.POP as ResidentUsersPerTotalPopulation,
+	Polygon.Shape.STArea() as  AREA_in_SQM,
+	Polygon.Shape
+FROM
+	[dbo].[CHINA_STREET_BLOCK_DISSOLVED_BY_THIESSEN_ID] as Polygon
+LEFT OUTER JOIN
+	temp1
+ON	
+	temp1.[Input_FID] = Polygon.[Input_FID]
+LEFT OUTER JOIN
+	temp2
+ON	
+	temp2.[Input_FID] = Polygon.[Input_FID]
+LEFT OUTER JOIN
+	CHINA_CENSUS_POPCENSUS2010_TOWNSHIP_THIESSEN as CENSUS
+ON	
+	CENSUS.[OBJECTID] = Polygon.[Input_FID]
+WHERE 
+	Polygon.ADM_2 = 262
+
+
+select distinct Shape.STSrid from [dbo].[CHINA_STREET_BLOCK_DISSOLVED_BY_THIESSEN_ID]
 
 
 -------------------
